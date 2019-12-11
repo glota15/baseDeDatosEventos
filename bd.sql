b4e4a91eafaf8166d4a943edcdad8e58df7348ad
glota15 <56084218+glota15@users.noreply.github.com>
2019-12-11 11:43:28 -0500

    version1.1

---
+++ bd.sql
@@ -0,0 +1,13 @@
+create table vela(
+    codigo int,
+    olor varchar(30),
+    tamanio varchar(3),
+    color varchar(50)
+);
+
+insert into vela (codigo,olor,tamanio,color) values
+(230, 'canela', 'mediana', 'blanco'),
+(231, 'vanilla', 'grande', 'verde'),
+(232, 'lavanda', 'pequeña', 'azul'),
+(233, 'chocolate', 'mediana', 'rosado'),
+(234, 'frutosRojos', 'grande', 'rojo'),
